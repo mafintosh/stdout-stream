178b973674b6c35ac9e51f3e9b993a693e254da3
Mathias Buus <mathiasbuus@gmail.com>
2014-04-18 10:02:13 +0200

    fix eagain issues

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var stream = require('stream');
 
 var exists = function(path) {
 	try {
@@ -11,12 +12,40 @@ var exists = function(path) {
 module.exports = function() {
 	if (!exists('/dev/stdout')) return process.stdout;
 
-	var stream = fs.createWriteStream('/dev/stdout', {fd:1});
-	stream._isStdio = true;
-	stream.isTTY = process.stdout.isTTY;
-	stream.end = function() {
+	var s = new stream.Writable({highWaterMark:0});
+
+	var cb;
+	var data;
+	var tries = 0;
+	var offset = 0;
+
+	var write = function() {
+		fs.write(1, data, offset, data.length - offset, null, onwrite);
+	};
+
+	var onwrite = function(err, written) {
+		if (err && err.code === 'EAGAIN' && tries++ < 30) return setTimeout(write, 10);
+		if (err) return cb(err);
+
+		tries = 0;
+		if (offset + written >= data.length) return cb();
+
+		offset = written;
+		write();
+	};
+
+	s._write = function(_data, enc, _cb) {
+		offset = 0;
+		cb = _cb;
+		data = _data;
+		write();
+	};
+
+	s._isStdio = true;
+	s.isTTY = process.stdout.isTTY;
+	s.end = function() {
 		throw new Error('process.stdout cannot be closed.');
 	};
 
-	return stream;
+	return s;
 }();
